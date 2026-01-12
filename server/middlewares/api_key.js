2080481773646925ab324396f70cec481e402024
mian95 <umairim24@gmail.com>
2026-01-13 00:02:02 +0500

    apikey_variable

--- server/middlewares/api_key.js
+++ server/middlewares/api_key.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 
 const apiKeyMiddleware = (req, res, next) => {
+
   const clientKey = req.headers["x-api-key"];
   const serverKey = process.env.API_KEY;
+ 
 
   if (!serverKey) {
     console.error("API_KEY is missing in environment");
