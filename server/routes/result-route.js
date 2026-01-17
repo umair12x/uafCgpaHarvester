2fb15858512c855e38b670606387c56c35ea11c6
mian95 <umairim24@gmail.com>
2026-01-17 23:07:46 +0500

    move the backend

--- server/routes/result-route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const Router = express.Router();
-const { homeHandler, resultHandler } = require("../controllers/result-control");
-
-Router.get("/", homeHandler);
-Router.post("/result", resultHandler);
-
-module.exports = Router;
